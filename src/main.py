1fb9becefe51060083c0b3057d1ea79a7c3149ae
Ace <ctrlaltmarshmallow@gmail.com>
2023-01-13 00:03:02 +0000

    UI Work + 2 New todos to finish on Friday (hopefully)

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ class MainPage(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.parent.geometry('750x575')
+        self.parent.resizable(False, False)
 
         self.preview_canvas  = tk.Canvas(self.parent, bg="grey")
         self.colormap_canvas = tk.Canvas(self.parent, bg="grey")
@@ -52,42 +53,61 @@ class MainPage(tk.Frame):
         logging.debug("MainPage initialized")
 
     def save_colormap(self):
-        pass
+        logging.debug("User pressed button - 'Save Colormap'")
 
     def load_colormap(self):
-        pass
+        logging.debug("User pressed button - 'Load Colormap'")
 
     def edit_colormap(self):
-        pass
+        logging.debug("User pressed button - 'Edit Colormap'")
 
     def reset_colormap(self):
-        pass
+        logging.debug("User pressed button - 'Reset Colormap'")
 
     def edit_vwin(self):
-        pass
+        logging.debug("User pressed button - 'Edit Vwin'")
 
     def supersampling(self):
-        pass
+        logging.debug("User pressed button - 'Supersampling'")
 
     def save_project(self):
-        pass
+        logging.debug("User pressed button - 'Save Project'")
 
     def load_project(self):
-        pass
+        logging.debug("User pressed button - 'Load Project'")
 
     def settings(self):
-        pass
+        logging.debug("User pressed button - 'Settings'")
 
     def parameters(self):
-        pass
-
+        logging.debug("User pressed button - 'Parameters'")
+        logging.debug("Trying to open new window...")
+        param_window = tk.Toplevel(self.parent)
+        param_app = ParameterSettings(param_window)
     def render(self):
-        pass
+        logging.debug("User pressed button - 'Render'")
 
     def video(self):
-        pass
+        logging.debug("User pressed button - 'Video mode settings'")
 
 
+class ParameterSettings:
+    def __init__(self, parent):
+        self.parent = parent
+        self.frame = tk.Frame(self.parent)
+        self.parent.geometry('400x200')
+        self.parent.resizable(False, False)
+
+        #todo: text box
+        self.exit_button = tk.Button(self.parent, bg="grey", text="Save and exit", command=self.exit_window)
+
+        self.exit_button.place(x=12, y=147, height=45, width=375)
+
+        logging.debug("ParameterSettings window initalized.")
+    def exit_window(self):
+        #todo: save here
+        logging.debug("Closing ParameterSettings window...")
+        self.parent.destroy()
 def main():
     initialise_logger(True) #change to false during prod
     structure_check(getcwd())
